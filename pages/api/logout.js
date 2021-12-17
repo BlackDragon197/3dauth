8bb7e377062e6cb2a1bc665f849da1bde100ed3b
imechko <1@mail.nett>
2021-12-17 09:10:52 +0200

    3dauth

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,10 +1,11 @@
 import Cookies from "cookies";
-
+import Router from 'next/router';
 export default function logout(req, res){
 
     var keys = ['keyboard cat']
     const cookies = new Cookies(req, res, { keys: keys })
 
     cookies.set('token')
+    cookies.set('token.sig')
     res.sendStatus(204);
 }
